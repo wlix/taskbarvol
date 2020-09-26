2e671a6e272241268258db77f3d2ce330c8ebbb5
wlix <tsubasa.ie.wlix@gmail.com>
2020-09-27 00:55:33 +0900

    change readme and version

--- resource.h
+++ resource.h
@@ -1,16 +1,4 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ で生成されたインクルード ファイル。
-// taskbarvol.rc で使用
-//
-#define IDC_STATIC                      -1
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
